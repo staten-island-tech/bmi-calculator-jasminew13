f64e31c429c1dfbe386be133335b9d905f5d48a2
jasminew13 <jasminewu101@gmail.com>
2023-10-04 10:13:44 -0400

    LIHERWHTOI3GLK

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-function divide(x,y){
-    console.log(x / y);     
+    const w = Number(prompt("enter your weight"));
+    const h = Number(prompt("enter your height in cm"));
+
+function bmicalculator(w,h){
+    console.log((703 * w) / (h*h));     
  }
- 
- function test(){
-    //local variable function scope
-    const x = Number(prompt("enter your weight"));
-    const y = Number(prompt("enter your height in inches"));
-    divide(x,y);
-}
+
+ function checkbmi(){
+    
+ }
+
+ function ok(){
+    let BMI = bmicalculator(w,h);
+    checkbmi(BMI);
+    if (BMI < 18.5){
+        console.log("underweight");
+    }else if (BMI >= 24.9 && BMI >18.5){
+        console.log("healthy");
+    }else if (BMI > 30){
+        console.log("overweight");
+    }
+    }
